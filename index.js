80b6ac7e0f353ab142a8271100e015bcd2b5c4d9
Andrew Venson <vensoneandrew@gmail.com>
2020-06-18 18:06:01 -0500

    append results from answers to new object

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let inquirer = require("inquirer");
+
+var gitInfo = {};
 inquirer
   .prompt([
     /* Pass your questions in here */
@@ -13,7 +15,7 @@ inquirer
       type: "password",
     },
     {
-      name: "repo",
+      name: "repotype",
       message: "Choose one: ",
       choices: function () {
         return [
@@ -25,18 +27,54 @@ inquirer
     },
   ])
   .then((answers) => {
-    if (answers.repo === "Create a new repo with Readme") {
-      return inquirer.prommpt([
+    gitInfo = answers;
+    if (answers.repotype === "Create a new repo with Readme") {
+      return inquirer.prompt([
         {
-          name: "repoName",
+          name: "reponame",
           message: "Enter new repo name: ",
+          type: "input",
+        },
+        {
+          name: "description",
+          message: "Description: ",
+          type: "input",
+        },
+        {
+          name: "toc",
+          message: "Table of Contents: ",
+          type: "input",
+        },
+        {
+          name: "install",
+          message: "Installation: ",
+          type: "input",
+        },
+        {
+          name: "usage",
+          message: "Usage: ",
+          type: "input",
+        },
+        {
+          name: "lic",
+          message: "License: ",
+          type: "input",
+        },
+        {
+          name: "contrib",
+          message: "Contributing: ",
+          type: "input",
+        },
+        {
+          name: "tests",
+          message: "Tests: ",
           type: "input",
         },
       ]);
     } else {
       return inquirer.prompt([
         {
-          name: "repoName",
+          name: "reponame",
           message: "Enter existing repo name: ",
           type: "input",
         },
@@ -45,4 +83,17 @@ inquirer
   })
   .then((answers) => {
     // Use user feedback for... whatever!!
+    appendResults(answers);
   });
+
+function appendResults(answers) {
+  gitInfo.reponame = answers.reponame;
+  gitInfo.description = answers.description;
+  gitInfo.toc = answers.toc;
+  gitInfo.install = answers.install;
+  gitInfo.usage = answers.usage;
+  gitInfo.lic = answers.lic;
+  gitInfo.contrib = answers.contrib;
+  gitInfo.tests = answers.tests;
+  console.log(gitInfo);
+}
