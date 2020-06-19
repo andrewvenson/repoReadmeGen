831c28d20e32f5c2a9f8488e7f15d328c30f555b
andrewvenson <vensoneandrew@gmail.com>
2020-06-19 17:39:33 -0500

    authentication completed

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-let inquirer = require("inquirer");
+var inquirer = require("inquirer");
+var github = require("octonode");
 
-var gitInfo = {};
+var authenticated = false;
 
-let asyncPrompt = async function () {
-  var user;
-  var pass;
-
+let main = async function () {
   let { username, password } = await inquirer.prompt([
     {
       name: "username",
@@ -19,104 +17,24 @@ let asyncPrompt = async function () {
     },
   ]);
 
-  user = username;
-  pass = password;
-
-  while (user !== "andrewvenson" && pass !== "Hooper33!@") {
-    console.log("Invalid credentials, please try again");
-    let { username, password } = await inquirer.prompt([
-      {
-        name: "username",
-        message: "Github Username: ",
-        type: "input",
-      },
-      {
-        name: "password",
-        message: "Github Password: ",
-        type: "password",
-      },
-    ]);
-    user = username;
-    pass = password;
-  }
-
-  console.log("valid credentials", user, pass);
-  console.log(user, pass);
+  let client = github.client({
+    username: username,
+    password: password,
+  });
+  asyncClient(client);
 };
 
-asyncPrompt();
-
-// inquirer
-//   .prompt([
-//     /* Pass your questions in here */
-// {
-//   name: "username",
-//   message: "Github Username: ",
-//   type: "input",
-// },
-// {
-//   name: "password",
-//   message: "Github Password: ",
-//   type: "password",
-// },
-// {
-//   name: "repotype",
-//   message: "Choose one: ",
-//   choices: function () {
-//     return [
-//       "Create a new repo with Readme",
-//       "Re-create existing repo's Readme",
-//     ];
-//   },
-//   type: "list",
-// },
-//   ])
-//   .then((answers) => {
-//     gitInfo = answers;
-//     if (answers.repotype === "Create a new repo with Readme") {
-//       let toContent = null;
-//       while (toContent != "done") {
-//         return inquirer.prompt([
-//           {
-//             name: "reponame",
-//             message: "Enter new repo name: ",
-//             type: "input",
-//           },
-//           {
-//             name: "description",
-//             message: "Description: ",
-//             type: "input",
-//           },
-//           {
-//             name: "toc",
-//             message: "Table of Contents: ",
-//             type: "input",
-//           },
-//         ]);
-//       }
-//     } else {
-//       return inquirer.prompt([
-//         {
-//           name: "reponame",
-//           message: "Enter existing repo name: ",
-//           type: "input",
-//         },
-//       ]);
-//     }
-//   })
-//   .then((answers) => {
-//     // Use user feedback for... whatever!!
-//     appendResults(answers);
-//   });
-
-function appendResults(answers) {
-  gitInfo.reponame = answers.reponame;
-  gitInfo.description = answers.description;
-  gitInfo.toc = answers.toc;
-  gitInfo.install = answers.install;
-  gitInfo.usage = answers.usage;
-  gitInfo.lic = answers.lic;
-  gitInfo.contrib = answers.contrib;
-  gitInfo.tests = answers.tests;
-  console.log(gitInfo);
+function asyncClient(client) {
+  client.get("/user", {}, function (err, status, body, headers) {
+    if (!err) {
+      authenticated = true;
+      console.log("authenticated");
+    } else {
+      authenticated = false;
+      console.log("Invalid credentials");
+      main();
+    }
+  });
 }
+
+main();
