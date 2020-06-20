2e15bbeda70c65a4ee9c786825f5c7d6806bf819
andrewvenson <vensoneandrew@gmail.com>
2020-06-19 20:29:03 -0500

    add installation prompts for readmeObj

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var github = require("octonode");
 
 var authenticated = false;
 var gitClient;
+var readmeObj = {
+  toc: ["Installation", "Usage", "License", "Contributing", "Tests"],
+  installstep: [],
+};
 
 let main = async function () {
   let { username, password } = await inquirer.prompt([
@@ -58,8 +62,115 @@ function initialPrompts() {
       },
     ])
     .then((answers) => {
-      console.log(answers.create);
+      if (answers.create === "Create new Repo with Readme") {
+        newRepoPrompts();
+      } else {
+        console.log("modifying existing readme");
+      }
     });
+}
+
+function newRepoPrompts() {
+  inquirer
+    .prompt([
+      {
+        name: "reponame",
+        type: "input",
+        message: "Repo name: ",
+      },
+      {
+        name: "description",
+        type: "input",
+        message: "Description: ",
+      },
+      {
+        name: "installation",
+        type: "list",
+        message: "Would you like to add installation steps?",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then((answers) => {
+      readmeObj.reponame = answers.reponame;
+      readmeObj.description = answers.description;
+
+      if (answers.installation === "Yes") {
+        installationSteps();
+      } else {
+        console.log("go to usage");
+      }
+    });
+}
+
+function installationSteps() {
+  inquirer
+    .prompt([
+      {
+        name: "installationstep",
+        type: "list",
+        message: "Step type: ",
+        choices: ["Add installation step", "Add installation code snippet"],
+      },
+    ])
+    .then((answers) => {
+      if (answers.installationstep === "Add installation step") {
+        inquirer
+          .prompt([
+            {
+              name: "installstep",
+              type: "input",
+              message: "Enter step: ",
+            },
+            {
+              name: "continue",
+              type: "list",
+              message:
+                "Would you like to continue adding to installation steps?",
+              choices: ["Yes", "No"],
+            },
+          ])
+          .then((answer) => {
+            if (answer.continue === "Yes") {
+              readmeObj.installstep.push({ textstep: answer.installstep });
+              installationSteps();
+            } else {
+              readmeObj.installstep.push({ textstep: answer.installstep });
+
+              usage();
+            }
+          });
+      } else {
+        inquirer
+          .prompt([
+            {
+              name: "codestep",
+              type: "input",
+              message: "Enter step: ",
+            },
+            {
+              name: "continue",
+              type: "list",
+              message:
+                "Would you like to continue adding to installation steps?",
+              choices: ["Yes", "No"],
+            },
+          ])
+          .then((answer) => {
+            if (answer.continue === "Yes") {
+              readmeObj.installstep.push({ codestep: answer.codestep });
+              installationSteps();
+            } else {
+              readmeObj.installstep.push({ codestep: answer.codestep });
+              usage();
+            }
+          });
+      }
+    });
+}
+
+function usage() {
+  console.log(readmeObj);
+  console.log("adding usage");
 }
 
 main();
